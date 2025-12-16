db92a03a1b8731effa47e4ebec59817fe7a7378c
geekiot <geekiot@proton.me>
2025-12-17 04:40:02 +0500

    Update

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "debug_toolbar",
     "django_bootstrap5",
 ]
 
@@ -37,7 +36,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 INTERNAL_IPS = [
